5884e0a6c2cafe9bbe6765739d2cb42b4f75969e
rebojuliansyah <rebojuliansyah@gmail.com>
2024-10-13 03:03:42 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./spectre-miner   --mining-address=spectre:qpuyp755lgfxmtn9savr253puuupuuradk9sayn0jkvhul7xpymu6azphncd4  --spectred-address=139.59.242.71 --port=5555 --threads=2
+./spectre-miner --mining-address=spectre:qpuyp755lgfxmtn9savr253puuupuuradk9sayn0jkvhul7xpymu6azphncd4  --spectred-address=139.59.242.71 --threads=2
 pause
